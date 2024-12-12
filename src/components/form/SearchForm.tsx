6980da9630bb7a61ed48479d907a3c3ec3fe87ba
Priydarshini Karanth <kpriya1203@gmail.com>
2024-12-12 16:35:12 +0530

    search form ui setup

--- src/components/form/SearchForm.tsx
+++ src/components/form/SearchForm.tsx
@@ -1,10 +1,49 @@
+import { type FormEvent, useState } from 'react';
+import { Label } from '@/components/ui/label';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { useToast } from '@/hooks/use-toast';
+
 type SearchFormProps = {
   userName: string;
   setUserName: React.Dispatch<React.SetStateAction<string>>;
 };
 
 function SearchForm({ userName, setUserName }: SearchFormProps) {
-  return <div>Search Form - {userName}</div>;
+  const [text, setText] = useState(userName);
+  const { toast } = useToast();
+
+  const handleSearch = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (text === '') {
+      toast({
+        description: 'Please enter a valid username',
+        variant: 'destructive',
+      });
+      return;
+    }
+    setUserName(text);
+  };
+
+  return (
+    <form
+      onSubmit={handleSearch}
+      className="flex items-center gap-x-2 w-full lg:w-1/3 mb-8"
+    >
+      <Label htmlFor="search" className="sr-only">
+        Search
+      </Label>
+      <Input
+        type="text"
+        id="search"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Search Github User..."
+        className="bg-background flex-grow"
+      />
+      <Button type="submit">Submit</Button>
+    </form>
+  );
 }
 
 export default SearchForm;
