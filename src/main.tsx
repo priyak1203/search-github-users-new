6e897fe725ef96f06a7394e46ad978050b0c1919
Priydarshini Karanth <kpriya1203@gmail.com>
2024-12-14 23:57:46 +0530

    graphql and apollo client setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { Toaster } from '@/components/ui/toaster.tsx';
+import { ApolloProvider } from '@apollo/client';
+import client from './apolloClient.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    <Toaster />
+    <ApolloProvider client={client}>
+      <App />
+      <Toaster />
+    </ApolloProvider>
   </StrictMode>
 );
