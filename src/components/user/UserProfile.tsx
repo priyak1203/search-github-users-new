a637e03967b038934ecfd55c8b5043caf48c9ca9
Priydarshini Karanth <kpriya1203@gmail.com>
2024-12-15 00:16:21 +0530

    query setup in user profile

--- src/components/user/UserProfile.tsx
+++ src/components/user/UserProfile.tsx
@@ -1,9 +1,29 @@
+import { GET_USER } from '@/queries';
+import { UserData } from '@/types';
+import { useQuery } from '@apollo/client';
+
 type UserProfileProps = {
   userName: string;
 };
 
 function UserProfile({ userName }: UserProfileProps) {
-  return <h1 className="text-2xl font-bold">UserProfile - {userName}</h1>;
+  const { loading, error, data } = useQuery<UserData>(GET_USER, {
+    variables: { login: userName },
+  });
+
+  if (loading) return <div>Loading...</div>;
+
+  if (error) return <h2 className="text-xl">{error.message}</h2>;
+
+  if (!data) return <h2 className="text-xl">User Not Found.</h2>;
+
+  const { bio } = data.user;
+
+  return (
+    <div>
+      <h1>{bio}</h1>
+    </div>
+  );
 }
 
 export default UserProfile;
